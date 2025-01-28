aee64029f8e1a8796fe02ea50885c1e19d17e420
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2025-01-28 10:24:25 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,3 @@
+snowflake://admin:pav18w345@hosti.us-east-12:4443/ealkigfgit
+mongodb://myDBReader:D1fficultP%40ssw0rd@mongodb0.com:27017/?authSource=admin
+postgres://user:pass123456@localhost:5500
