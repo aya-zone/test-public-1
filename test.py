e7569f9156947850f0182092c13ee6d812ae831c
aya-apiiro <143873580+aya-apiiro@users.noreply.github.com>
2025-02-20 16:58:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,4 @@ snowflake://admin:pav18w345@hosti.us-east-12:4443/ealkigfgit
 mongodb://myDBReader:D1fficultP%40ssw0rd@mongodb0.com:27017/?authSource=admin
 postgres://user:pass123456@localhost:5500
 ion_YfXkouFxPt4X9YfvFmplWIUxo1JW7VVPredK0LHyJx
+MapboxAccessToken= 'pk.eyJ1IjoiaGlqaWFuZ3RhbyIsImEiOiJjampxcjFnb3E2NTB5M3BvM253ZHV5YjhjIn0.WneUon5qFigfJRJ3oaZ3Ow'
